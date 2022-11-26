cd266f078d60e7a0a3709bb49c62918659931978
BijonTalukder <bijotalukder1247@gmail.com>
2022-11-26 13:06:35 +0600

    done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app= express();
 require('dotenv').config();
@@ -12,11 +12,36 @@ console.log(process.env.DB_USER)
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ywgnkn8.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+async function run(){
+    try{
+        const database=client.db('FlowByte')
+        const categories=database.collection('categories');
+        const userCollection=database.collection('userCollection');
+        app.get('/category/:id',async(req,res)=>{
+            const id=parseInt( req.params.id);
+            console.log(id)
+            const query={categoryId:id}
+            const cursor=categories.find(query)
+            const result=await cursor.toArray();
+            console.log(query)
+            res.send(result)
+            console.log(result)
+
+        })
+        app.post('/users',async(req,res)=>{
+            const user=req.body;
+            const result=await userCollection.insertOne(user)
+            res.send(result)
+
+        })
+
+    }
+    catch{
+
+    }
+
+}
+run().catch(console.log)
 
 app.get('/',async(req,res)=>{
     res.send('runnig')
